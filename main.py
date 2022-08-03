23cc3d425b39f7a50d7c66afed352e61f51fb452
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-08-03 16:18:07 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,76 +1,77 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+def on_button_pressed_a():
+    global matka_location
     matka_location += 1
-    if (matka_location > 5) {
+    if matka_location > 5:
         matka_location = 1
-    }
-    
-})
-function draw_matka() {
-    if (matka_location == 0) {
-        basic.showLeds(`
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def draw_matka():
+    if matka_location == 0:
+        basic.show_leds("""
             . . . . .
                         . . . . .
                         . . . . .
                         . . . . .
                         . . . . .
-        `)
-    } else if (matka_location == 1) {
-        basic.showLeds(`
+        """)
+    else:
+        if matka_location == 1:
+            basic.show_leds("""
                 . . . . .
                                 . . . . .
                                 . . . . .
                                 . . . . .
                                 . . # # #
-            `)
-    } else if (matka_location == 2) {
-        basic.showLeds(`
+            """)
+        else:
+            if matka_location == 2:
+                basic.show_leds("""
                     . . . . .
                                         . . . . .
                                         . . . . .
                                         . . . . .
                                         . # # # .
-                `)
-    } else if (matka_location == 3) {
-        basic.showLeds(`
+                """)
+            else:
+                if matka_location == 3:
+                    basic.show_leds("""
                         . . . . .
                                                 . . . . .
                                                 . . . . .
                                                 . . . . .
                                                 # # # . .
-                    `)
-    } else if (matka_location == 4) {
-        basic.showLeds(`
+                    """)
+                else:
+                    if matka_location == 4:
+                        basic.show_leds("""
                             . . . . .
                                                         . . . . .
                                                         . . . . .
                                                         . . . . .
                                                         # # . . #
-                        `)
-    } else if (matka_location == 5) {
-        basic.showLeds(`
+                        """)
+                    else:
+                        if matka_location == 5:
+                            basic.show_leds("""
                                 . . . . .
                                                                 . . . . .
                                                                 . . . . .
                                                                 . . . . .
                                                                 # . . # #
-                            `)
-    } else {
-        basic.showNumber(matka_location)
-    }
-    
-}
+                            """)
+                        else:
+                            basic.show_number(matka_location)
 
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+def on_button_pressed_b():
+    global matka_location
     matka_location += -1
-    if (matka_location < 1) {
+    if matka_location < 1:
         matka_location = 5
-    }
-    
-})
-let matka_location = 0
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
 matka_location = 0
-basic.forever(function on_forever() {
+matka_location = 0
+
+def on_forever():
     draw_matka()
-})
+basic.forever(on_forever)
