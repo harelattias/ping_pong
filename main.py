992a1db2a250e47961241ea31b7541af1dd53381
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-08-15 11:52:36 +0200

    קיצור קיבה נוסף דרך שארית

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 matka_location = 0
 
-
 def on_button_pressed_a():
     global matka_location
-    matka_location += -1
-    if matka_location < 0:
-        matka_location = 4
+    matka_location = calculate_racket_location(-1 + matka_location)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def calculate_racket_location(loc: number):
-    if loc > 4:
-        loc = 0
-    elif loc < 0:
-        loc = 4
-    return loc
-
-
+    return (loc + 5) % 5
+    
 def draw_matka():
-    led.plot(0 + matka_location, 4)
-    led.plot(1 + matka_location, 4)
-    led.plot(2 + matka_location, 4)
+    led.plot(calculate_racket_location(0 + matka_location), 4)
+    led.plot(calculate_racket_location(1 + matka_location), 4)
+    led.plot(calculate_racket_location(2 + matka_location), 4)
 
 def on_button_pressed_b():
     global matka_location
--- main.ts
+++ main.ts
@@ -1,20 +1,14 @@
 let matka_location = 0
-let loc = 0
 input.onButtonPressed(Button.A, function () {
     matka_location = calculate_racket_location(-1 + matka_location)
 })
 function calculate_racket_location (loc: number) {
-    if (loc > 4) {
-        loc = 0
-    } else if (loc < 0) {
-        loc = 4
-    }
-    return loc
+    return (loc + 5) % 5
 }
 function draw_matka () {
-    led.plot(0 + matka_location, 4)
-    led.plot(1 + matka_location, 4)
-    led.plot(2 + matka_location, 4)
+    led.plot(calculate_racket_location(0 + matka_location), 4)
+    led.plot(calculate_racket_location(1 + matka_location), 4)
+    led.plot(calculate_racket_location(2 + matka_location), 4)
 }
 input.onButtonPressed(Button.B, function () {
     matka_location = calculate_racket_location(1 + matka_location)
