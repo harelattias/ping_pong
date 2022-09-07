5f90d99d1d69c97f148bf6db43de67e6cfda8bb2
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-09-07 16:50:43 +0200

    end game does not work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-matka_location = 0
+def draw_ball():
+    led.plot(ball_loc_x, ball_loc_y)
 
 def on_button_pressed_a():
     global matka_location
@@ -7,7 +8,6 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def calculate_racket_location(loc: number):
     return (loc + 5) % 5
-    
 def draw_matka():
     led.plot(calculate_racket_location(0 + matka_location), 4)
     led.plot(calculate_racket_location(1 + matka_location), 4)
@@ -18,7 +18,34 @@ def on_button_pressed_b():
     matka_location = calculate_racket_location(1 + matka_location)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def calc_ball_loc():
+    global ball_loc_x, ball_loc_y, ball_velocity_y, ball_velocity_x
+    ball_loc_x = ball_velocity_x + ball_loc_x
+    ball_loc_y = ball_velocity_y + ball_loc_y
+    if 3 == ball_loc_y:
+        if matka_location:
+            pass
+        ball_velocity_y = -1
+    if 0 == ball_loc_y:
+        ball_velocity_y = 1
+    if 4 == ball_loc_x:
+        ball_velocity_x = -1
+    if 0 == ball_loc_x:
+        ball_velocity_x = 1
+matka_location = 0
+ball_velocity_y = 0
+ball_velocity_x = 0
+ball_loc_y = 0
+ball_loc_x = 0
+ball_loc_x = 2
+ball_loc_y = 1
+ball_velocity_x = 1
+ball_velocity_y = 1
+
 def on_forever():
     basic.clear_screen()
     draw_matka()
+    draw_ball()
+    calc_ball_loc()
+    basic.pause(500)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-let matka_location = 0
+function draw_ball () {
+    led.plot(ball_loc_x, ball_loc_y)
+}
 input.onButtonPressed(Button.A, function () {
     matka_location = calculate_racket_location(-1 + matka_location)
 })
@@ -13,7 +15,55 @@ function draw_matka () {
 input.onButtonPressed(Button.B, function () {
     matka_location = calculate_racket_location(1 + matka_location)
 })
+function calc_ball_loc () {
+    ball_loc_x = ball_velocity_x + ball_loc_x
+    ball_loc_y = ball_velocity_y + ball_loc_y
+    if (3 == ball_loc_y) {
+        if (ball_loc_x == 0 + matka_location) {
+            ball_velocity_x = -1
+            ball_velocity_y = -1
+        } else {
+            if (ball_loc_x == 1 + matka_location) {
+                ball_velocity_x = 0
+                ball_velocity_y = -1
+            } else {
+                if (ball_loc_x == 2 + matka_location) {
+                    ball_velocity_x = 1
+                    ball_velocity_y = -1
+                } else {
+                    the_end_game = 8
+                }
+            }
+        }
+    }
+    if (0 == ball_loc_y) {
+        ball_velocity_y = 1
+    }
+    if (4 == ball_loc_x) {
+        ball_velocity_x = -1
+    }
+    if (0 == ball_loc_x) {
+        ball_velocity_x = 1
+    }
+}
+let the_end_game = 0
+let matka_location = 0
+let ball_velocity_y = 0
+let ball_velocity_x = 0
+let ball_loc_y = 0
+let ball_loc_x = 0
+ball_loc_x = 2
+ball_loc_y = 1
+ball_velocity_x = 1
+ball_velocity_y = 1
 basic.forever(function () {
     basic.clearScreen()
     draw_matka()
+    draw_ball()
+    calc_ball_loc()
+    if (8 == the_end_game) {
+        basic.showIcon(IconNames.Sad)
+        music.playMelody("C5 C D E F G A B ", 120)
+    }
+    basic.pause(1000)
 })
