9ddca15dab0faf0a644f80fe4c4c3e25b38140ae
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-09-21 16:31:43 +0200

    3 lives + bug fix

--- main.py
+++ main.py
@@ -19,47 +19,56 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def calc_ball_loc():
-    global ball_loc_x, ball_loc_y, ball_velocity_x, ball_velocity_y, the_end_game
-    ball_loc_x = ball_velocity_x + ball_loc_x
-    ball_loc_y = ball_velocity_y + ball_loc_y
+    global ball_velocity_x, ball_velocity_y, the_end_game, num_of_fails, ball_loc_x, ball_loc_y
     if 3 == ball_loc_y:
         if ball_loc_x == 0 + matka_location:
             ball_velocity_x = -1
             ball_velocity_y = -1
+        elif ball_loc_x == 1 + matka_location:
+            ball_velocity_x = 0
+            ball_velocity_y = -1
+        elif ball_loc_x == 2 + matka_location:
+            ball_velocity_x = 1
+            ball_velocity_y = -1
         else:
-            if ball_loc_x == 1 + matka_location:
-                ball_velocity_x = 0
-                ball_velocity_y = -1
-            else:
-                if ball_loc_x == 2 + matka_location:
-                    ball_velocity_x = 1
-                    ball_velocity_y = -1
-                else:
-                    the_end_game = 8
+            the_end_game = 1
+            num_of_fails += 1
     if 0 == ball_loc_y:
         ball_velocity_y = 1
     if 4 == ball_loc_x:
         ball_velocity_x = -1
     if 0 == ball_loc_x:
         ball_velocity_x = 1
+    ball_loc_x = ball_velocity_x + ball_loc_x
+    ball_loc_y = ball_velocity_y + ball_loc_y
+def reset_game():
+    global ball_loc_x, ball_loc_y, ball_velocity_x, ball_velocity_y, the_end_game
+    ball_loc_x = 1
+    ball_loc_y = 1
+    ball_velocity_x = 1
+    ball_velocity_y = 1
+    the_end_game = 0
 the_end_game = 0
-matka_location = 0
 ball_velocity_y = 0
 ball_velocity_x = 0
+matka_location = 0
 ball_loc_y = 0
 ball_loc_x = 0
-ball_loc_x = 1
-ball_loc_y = 1
-ball_velocity_x = 1
-ball_velocity_y = 1
+reset_game()
+num_of_fails = 0
 
 def on_forever():
-    basic.clear_screen()
-    draw_matka()
-    draw_ball()
-    calc_ball_loc()
-    if 8 == the_end_game:
+    if num_of_fails == 3:
         basic.show_icon(IconNames.SAD)
-        music.play_melody("C5 C D E F G A B ", 120)
-    basic.pause(1000)
+        music.play_melody("C5 C D E F G A B ", 500)
+        basic.pause(2000)
+    else:
+        if 1 == the_end_game:
+            reset_game()
+        else:
+            basic.clear_screen()
+            draw_matka()
+            draw_ball()
+            calc_ball_loc()
+            basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-function draw_ball () {
+function draw_ball() {
     led.plot(ball_loc_x, ball_loc_y)
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
     matka_location = calculate_racket_location(-1 + matka_location)
 })
-function calculate_racket_location (loc: number) {
+function calculate_racket_location(loc: number): number {
     return (loc + 5) % 5
 }
-function draw_matka () {
+
+function draw_matka() {
     led.plot(calculate_racket_location(0 + matka_location), 4)
     led.plot(calculate_racket_location(1 + matka_location), 4)
     led.plot(calculate_racket_location(2 + matka_location), 4)
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
     matka_location = calculate_racket_location(1 + matka_location)
 })
-function calc_ball_loc () {
-    ball_loc_x = ball_velocity_x + ball_loc_x
-    ball_loc_y = ball_velocity_y + ball_loc_y
+function calc_ball_loc() {
+    
     if (3 == ball_loc_y) {
         if (ball_loc_x == 0 + matka_location) {
             ball_velocity_x = -1
@@ -29,37 +33,58 @@ function calc_ball_loc () {
             ball_velocity_x = 1
             ball_velocity_y = -1
         } else {
-            the_end_game = 8
+            the_end_game = 1
+            num_of_fails += 1
         }
+        
     }
+    
     if (0 == ball_loc_y) {
         ball_velocity_y = 1
     }
+    
     if (4 == ball_loc_x) {
         ball_velocity_x = -1
     }
+    
     if (0 == ball_loc_x) {
         ball_velocity_x = 1
     }
+    
+    ball_loc_x = ball_velocity_x + ball_loc_x
+    ball_loc_y = ball_velocity_y + ball_loc_y
 }
+
+function reset_game() {
+    
+    ball_loc_x = 1
+    ball_loc_y = 1
+    ball_velocity_x = 1
+    ball_velocity_y = 1
+    the_end_game = 0
+}
+
 let the_end_game = 0
-let matka_location = 0
 let ball_velocity_y = 0
 let ball_velocity_x = 0
+let matka_location = 0
 let ball_loc_y = 0
 let ball_loc_x = 0
-ball_loc_x = 1
-ball_loc_y = 1
-ball_velocity_x = 1
-ball_velocity_y = 1
-basic.forever(function () {
-    basic.clearScreen()
-    draw_matka()
-    draw_ball()
-    calc_ball_loc()
-    if (8 == the_end_game) {
+reset_game()
+let num_of_fails = 0
+basic.forever(function on_forever() {
+    if (num_of_fails == 3) {
         basic.showIcon(IconNames.Sad)
-        music.playMelody("C5 C D E F G A B ", 120)
+        music.playMelody("C5 C D E F G A B ", 500)
+        basic.pause(2000)
+    } else if (1 == the_end_game) {
+        reset_game()
+    } else {
+        basic.clearScreen()
+        draw_matka()
+        draw_ball()
+        calc_ball_loc()
+        basic.pause(1000)
     }
-    basic.pause(1000)
+    
 })
